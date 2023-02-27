104c4fe845b076ec9aaee08e05452ea7d6f587f1
usagimaru <usagimaruma@gmail.com>
2023-02-27 17:34:55 +0900

    add

---
+++ Sources/EventTapWrapper.swift
@@ -0,0 +1,86 @@
+//
+//  EventTapWrapper.swift
+//
+//  Created by usagimaru on 2023/02/25.
+//
+//  References:
+//  https://stackoverflow.com/questions/9352939/how-can-one-detect-mission-control-or-command-tab-switcher-superseding-ones-pro
+//  https://stackoverflow.com/questions/55616797/how-can-i-pass-an-anonymous-method-into-a-closure-that-captures-context-in-swift
+//  https://github.com/briankendall/forceFullDesktopBar/blob/master/dockInjection/dockInjection.m
+//  https://github.com/wilix-team/iohook/blob/master/libuiohook/src/darwin/input_hook.c
+
+#if os(macOS)
+import Cocoa
+
+open class EventTapWrapper {
+	
+	typealias Handler = (CGEventTapProxy, CGEvent) -> ()
+	typealias EvaluationHandler = (EventTapWrapper, CGEvent) -> Bool
+	
+	open private(set) var identifier = UUID()
+	open private(set) var eventTypes = [CGEventType]()
+	open private(set) var tap: CFMachPort?
+	
+	private let handler: Handler
+	private let evaluationHandler: EvaluationHandler
+	
+	open init?(location: CGEventTapLocation,
+		  placement: CGEventTapPlacement,
+		  tapOption: CGEventTapOptions,
+		  eventTypes: [CGEventType],
+		  shouldCatch: @escaping EvaluationHandler,
+		  handler: @escaping Handler) {
+		self.handler = handler
+		self.evaluationHandler = shouldCatch
+		self.eventTypes = eventTypes
+		self.tap = nil
+		
+		let eventMask = eventTypes.reduce(CGEventMask(0), { $0 | (1 << $1.rawValue) })
+		
+		guard let tap = CGEvent.tapCreate(
+			tap: location,
+			place: placement,
+			options: tapOption,
+			eventsOfInterest: eventMask,
+			callback: { tapProxy, eventType, event, selfPointer in
+				let eventTapWrapper = Unmanaged<EventTapWrapper>.fromOpaque(selfPointer!).takeUnretainedValue()
+				let shoudCatch = eventTapWrapper.evaluationHandler(eventTapWrapper, event)
+				
+				eventTapWrapper.handler(tapProxy, event)
+				
+				// CGEventTapOptions.defaultTap にして nil を返すと beep が鳴らなくなるが、キーイベントを全て奪うので扱い注意
+				if shoudCatch == true {
+					return nil
+				}
+				return Unmanaged<CGEvent>.passUnretained(event)
+			},
+			userInfo: Unmanaged.passUnretained(self).toOpaque())
+		else { return nil }
+		
+		self.tap = tap
+	}
+	
+	open func addToRunLoop(_ runLoop: CFRunLoop = CFRunLoopGetCurrent(), mode: CFRunLoopMode = .commonModes) {
+		if let tap = self.tap {
+			CFRunLoopAddSource(
+				runLoop,
+				CFMachPortCreateRunLoopSource(kCFAllocatorDefault, tap, 0),
+				mode
+			)
+			
+			CGEvent.tapEnable(tap: tap, enable: true)
+			CFRunLoopRun()
+		}
+	}
+	
+	open func removeFromRunLoop(_ runLoop: CFRunLoop = CFRunLoopGetCurrent(), mode: CFRunLoopMode = .commonModes) {
+		if let tap = self.tap {
+			CFRunLoopRemoveSource(runLoop,
+								  CFMachPortCreateRunLoopSource(kCFAllocatorDefault, tap, 0),
+								  mode)
+		}
+	}
+	
+}
+
+#endif
---
+++ Sources/EventTapper.swift
@@ -0,0 +1,143 @@
+//
+//  EventWatcher.swift
+//
+//  Created by usagimaru on 2017.06.18.
+//  Copyright © 2017 usagimaru. All rights reserved.
+//
+
+#if os(macOS)
+import Cocoa
+
+@objc public protocol EventTapperDelegate: AnyObject {
+	
+	@objc optional func eventTapperDidCatchFlagsChanged(event: NSEvent)
+	@objc optional func eventTapperDidCatchKeyEvent(event: NSEvent, isDown: Bool)
+	
+	@objc optional func eventTapperDidCatchLeftMouseEvent(event: NSEvent, isDown: Bool)
+	@objc optional func eventTapperDidCatchLeftMouseDraggedEvent(event: NSEvent)
+	@objc optional func eventTapperDidCatchRightMouseEvent(event: NSEvent, isDown: Bool)
+	@objc optional func eventTapperDidCatchRightMouseDraggedEvent(event: NSEvent)
+	@objc optional func eventTapperDidCatchMouseMoved(event: NSEvent)
+	
+}
+
+open class EventTapper: NSObject {
+	
+	public static let shared = EventTapper()
+	public weak var delegate: EventTapperDelegate?
+	
+	public var eventTapHandler: ((_ event: CGEvent) -> Void)?
+	
+	private var tapWrappers = [[CGEventType] : EventTapWrapper]()
+	
+	open func tapEvents(for eventTypes: [CGEventType], evaluationHandler: @escaping (_ event: CGEvent) -> Bool) {
+		self.tapWrappers[eventTypes]?.removeFromRunLoop()
+		let tapWrapper = EventTapWrapper(location: .cghidEventTap,
+										 placement: .headInsertEventTap,
+										 tapOption: .defaultTap,
+										 eventTypes: eventTypes)
+		{ eventTapWrapper, event in
+			evaluationHandler(event)
+			
+		} handler: { eventTapProxy, event in
+			guard let nsevent = NSEvent(cgEvent: event)
+			else { return }
+			
+			self.eventTapHandler?(event)
+			
+			if event.type == .flagsChanged {
+				self.delegate?.eventTapperDidCatchFlagsChanged?(event: nsevent)
+			}
+			if event.type == .keyDown {
+				self.delegate?.eventTapperDidCatchKeyEvent?(event: nsevent, isDown: true)
+			}
+			if event.type == .keyUp {
+				self.delegate?.eventTapperDidCatchKeyEvent?(event: nsevent, isDown: false)
+			}
+			if event.type == .leftMouseDown {
+				self.delegate?.eventTapperDidCatchLeftMouseEvent?(event: nsevent, isDown: true)
+			}
+			if event.type == .leftMouseUp {
+				self.delegate?.eventTapperDidCatchLeftMouseEvent?(event: nsevent, isDown: false)
+			}
+			if event.type == .leftMouseDragged {
+				self.delegate?.eventTapperDidCatchLeftMouseDraggedEvent?(event: nsevent)
+			}
+			if event.type == .rightMouseDown {
+				self.delegate?.eventTapperDidCatchRightMouseEvent?(event: nsevent, isDown: true)
+			}
+			if event.type == .rightMouseUp {
+				self.delegate?.eventTapperDidCatchRightMouseEvent?(event: nsevent, isDown: false)
+			}
+			if event.type == .rightMouseDragged {
+				self.delegate?.eventTapperDidCatchRightMouseDraggedEvent?(event: nsevent)
+			}
+			if event.type == .mouseMoved {
+				self.delegate?.eventTapperDidCatchMouseMoved?(event: nsevent)
+			}
+		}
+		
+		if let tapWrapper {
+			self.tapWrappers[eventTypes] = tapWrapper
+			tapWrapper.addToRunLoop()
+		}
+	}
+	
+}
+
+
+// MARK: -
+
+public extension EventTapper {
+	
+	/// Simulate key event
+	public static func postKeyEvent(virtualKey: CGKeyCode,
+							 isKeyDown: Bool,
+							 settings: ((_ event: CGEvent) -> (Void))? = nil,
+							 eventSourceStateID: CGEventSourceStateID = .combinedSessionState,
+							 tapLocation: CGEventTapLocation = .cghidEventTap)
+	{
+		let source = CGEventSource(stateID: eventSourceStateID)
+		if let event = CGEvent(keyboardEventSource: source, virtualKey: virtualKey, keyDown: isKeyDown) {
+			settings?(event)
+			event.post(tap: tapLocation)
+		}
+	}
+	
+	/// Simulate key down event
+	public static func postKeyDown(key: CGKeyCode, flags: CGEventFlags = .maskNonCoalesced, keyDown: Bool = true) {
+		postKeyEvent(virtualKey: key, isKeyDown: keyDown) { event in
+			event.flags = flags // To override the modifier key being pressed by the user. (Do not insert new flags)
+		}
+	}
+	
+	/// Simulate mouse button event
+	public static func postMouseButtonEvent(mouseType: CGEventType,
+									 mouseButton: CGMouseButton,
+									 position: CGPoint? = nil,
+									 settings: ((_ event: CGEvent) -> (Void))? = nil,
+									 eventSourceStateID: CGEventSourceStateID = .combinedSessionState,
+									 tapLocation: CGEventTapLocation = .cghidEventTap)
+	{
+		let mousePosition = position ?? CGEvent(source: nil)?.location ?? .zero
+		let source = CGEventSource(stateID: eventSourceStateID)
+		if let event = CGEvent(mouseEventSource: source, mouseType: mouseType, mouseCursorPosition: mousePosition, mouseButton: mouseButton) {
+			settings?(event)
+			event.post(tap: tapLocation)
+		}
+	}
+	
+	/// Simulate primary mouse button click
+	public static func postPrimaryMouseClick() {
+		postMouseButtonEvent(mouseType: .leftMouseDown, mouseButton: .left)
+		postMouseButtonEvent(mouseType: .leftMouseUp, mouseButton: .left)
+	}
+	
+	/// Simulate secondary mouse button click
+	public static func postSecondaryMouseClick() {
+		postMouseButtonEvent(mouseType: .rightMouseDown, mouseButton: .right)
+		postMouseButtonEvent(mouseType: .rightMouseUp, mouseButton: .right)
+	}
+	
+}
+#endif
